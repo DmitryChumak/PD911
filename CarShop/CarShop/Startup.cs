9cd1ee45a71484d1a0e1178f57d304ddf9deb1c7
Dmitry <step@itstep.org>
2021-09-08 13:50:45 +0300

    JS client for API

--- CarShop/CarShop/Startup.cs
+++ CarShop/CarShop/Startup.cs
@@ -52,10 +52,13 @@ namespace CarShop
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+           
+        
             app.UseStatusCodePages("text/html", "Error <b>404</b>!");
-
+            app.UseHttpsRedirection();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+         
             app.UseRouting();
 
             app.UseAuthentication();
